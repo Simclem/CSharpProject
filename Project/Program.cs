da700e9b3a48e1a7a2fd96e195fe6ac588c2e5f2
Simclem <simon.clement98@yahoo.com>
2017-09-11 20:50:46 +0200

    Suppression de console.writeline

--- Project/Program.cs
+++ Project/Program.cs
@@ -19,89 +19,11 @@ namespace Project
             //Création des joueur de la partie
             List<Soldier> ListOfSoldier = InitSoldier(3);
             Wall DefenseWall = new Wall(10, 10);
-            List<Zombie> ListZombie = new List<Zombie>();
-            Zombie zombie = new Zombie(1, 1, 1, 1, 1, 1, "Zombie");
-            Zombie zomb = new Zombie(1, 1, 1, 1, 1, 1, "Zomb");
-            Zombie z = new Zombie(1, 1, 1, 1, 1, 1, "Z");
-            ListZombie.Add(zombie);
-            ListZombie.Add(zomb);
-            ListZombie.Add(z);
-            /* Console.WriteLine(ListOfSoldier[0].ToString());
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             ListOfSoldier[0].TakeDamage(1);
-             Console.WriteLine(ListOfSoldier[0].ToString());
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             ListOfSoldier[0].TakeHeal(12);
-             Console.WriteLine(ListOfSoldier[0].ToString());
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             ListOfSoldier[0].TakeLevel(20);
-
-             Console.WriteLine(ListOfSoldier[0].ToString());
-             Console.WriteLine("___________________________________________");
-             Zombie zombie = new Zombie(1, 1, 1, 1, 1, 1,"Zombie mort");
-             Console.WriteLine(zombie.ToString());
-
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             ListOfSoldier[0].Attack(zombie);
-             Console.WriteLine(zombie.ToString());
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine(DefenseWall.ToString());
-             zombie.Attack(ListOfSoldier[0], DefenseWall);
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine();
-             Console.WriteLine(DefenseWall.ToString());
-             Console.WriteLine(ListOfSoldier[0].ToString());
-             */
-
-            //on commence une vague
             Wave vagueTest = new Wave(1, 10);
+            vagueTest.Play(ListOfSoldier, DefenseWall);
             vagueTest.UpdateLevel(ListOfSoldier);
-            Console.WriteLine(ListOfSoldier[0].ToString());
-            Turn tour = new Turn(1);
-            tour.AttackPhase(ListOfSoldier, ListZombie, DefenseWall);
-
-
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Zombie " + zombie.ToString());
-            Console.WriteLine("Zomb " + zomb.ToString());
-            Console.WriteLine("Z " + z.ToString());
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine(ListOfSoldier[0].ToString());
-            Console.WriteLine();
-            Console.WriteLine(ListOfSoldier[1].ToString());
-            Console.WriteLine();
-            Console.WriteLine(ListOfSoldier[2].ToString());
-            //Console.WriteLine(vagueTest.ToString());
             Console.ReadLine();
-
+            
 
         }
 
